9c5e3640f10210c7b4663ee1e07f3525f896b2bb
sandlord54 <50053527+sandlord54@users.noreply.github.com>
2020-02-26 18:01:33 +0300

    heroku123

--- bot.py
+++ bot.py
@@ -3,27 +3,6 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_html(url):
-    result = requests.get(url)
-    return result.text
-
-def get_data(html):
-    global res
-    soup = BeautifulSoup(html,"html.parser")
-    h1 = soup.find('div', {'class':'weather__content_tab-temperature'})
-    opisanie = soup.find('div', {'class':'weather__article_description-text'})
-    temp = soup.find('div', {'class':'weather__article_main_temp'})
-    res = opisanie.text +'\n'+ 'Температура в Рыбинске сейчас:'+temp.text+h1.text
-    print(res)
-
-
-
-def glob():
-    html = get_html('https://sinoptik.com.ru/погода-рыбинск-100500004')
-    get_data(html)
-
-if __name__ == '__main__':
-    glob()
 
 
 TOKEN = '969612971:AAFxpDNtky1WBjuODbGbPja6j8vmDgHdNRo'
